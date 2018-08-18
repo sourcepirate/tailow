09cc1251a590195c0ef2fb428d04bec5b1fbec75
sourcepirate <sathyanarrayanan@yandex.com>
2018-08-18 18:11:38 +0800

    feat() added few more operators

--- tailow/operators/__init__.py
+++ tailow/operators/__init__.py
@@ -2,5 +2,11 @@
 
 from .base import OperationRegistry, Operator, Q
 from .inopr import InOperator
+from .gt import GTEOperator, GTOperator
+from .lt import LTEOperator, LTOperator
 
 OperationRegistry.register("in", InOperator)
+OperationRegistry.register("gte", GTEOperator)
+OperationRegistry.register("gt", GTOperator)
+OperationRegistry.register("lte", LTEOperator)
+OperationRegistry.register("lt", LTOperator)
