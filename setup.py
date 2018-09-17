c5e0bb73ecea675bc8150e6206e8f1d29d8c165f
sourcepirate <sathyanarrayanan@yandex.com>
2018-09-17 18:38:08 +0800

    chore(setup.py) bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
 
 setup(
     name='tailow',
-    version='0.1.0',
+    version='0.1.1',
     description='A async wrapper for mongodb',
     long_description=README,
     author='Sourcepirate',
